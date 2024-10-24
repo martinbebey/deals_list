e8c1a75164cdcd5018ae94888dcf7fc4ff9201e9
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-10-24 13:05:14 -0400

    added listing page item model + api service wip

---
+++ app/src/main/java/com/developer/dealslist/model/DataModels.kt
@@ -0,0 +1,31 @@
+package com.developer.dealslist.model
+
+data class ListingItem(
+    var aisle: String,
+    var availability: String,
+    var description: String,
+    var fulfillment: String,
+    var id: Int,
+    var imageUrl: String,
+    var regularPrice: Price,
+    var salePrice: Price,
+    var title: String
+)
+
+data class Price(
+    var amountInCents: Int,
+    var currencySymbol: String,
+    var displayingString: String
+)
+sealed interface ViewState {
+    data object Loading: ViewState
+
+    data class ItemNotFound(
+        val code: String,
+        val message: String
+    ): ViewState
+
+    data class Success(
+        val itemList: List<ListingItem>
+    ): ViewState
+}
---
+++ app/src/main/java/com/developer/dealslist/services/DealsListApiService.kt
@@ -0,0 +1,25 @@
+package com.developer.dealslist.services
+
+import com.developer.fetchapp.model.Item
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+const val BASE_URL = "https://fetch-hiring.s3.amazonaws.com/"
+const val END_POINT = "hiring.json"
+
+private val retrofit = Retrofit
+    .Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val fetchService = retrofit.create(ApiService::class.java)
+
+/**
+ * Service for fetching the remote data list
+ **/
+interface DealsListApiService{
+    @GET(END_POINT)
+    suspend fun getListItems(): MutableList<Item>
+}
